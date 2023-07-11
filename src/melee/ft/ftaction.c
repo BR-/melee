06e4d984263c823c4cdad7eef32cbb8e2b0a7de7
BR <wishingwellwellwishing@hotmail.com>
2023-07-11 09:49:59 -0700

    more documentation

--- src/melee/ft/ftaction.c
+++ src/melee/ft/ftaction.c
@@ -1,9 +1,23 @@
 // https://github.com/Ploaj/HSDLib/blob/939cc10be5a9f76fdcf3ca8ff4a54e35e545fe4a/HSDRawViewer/Scripts/command_fighter.yml
 // https://smashboards.com/threads/melee-hacks-and-you-new-hackers-start-here-in-the-op.247119/page-48#post-10769744
 // https://smashboards.com/threads/new-melee-syntax-school-you-can-write-character-commands-now.402587/
+// https://github.com/DRGN-DRC/Melee-Modding-Wizard/blob/main/FileSystem/charFiles.py#L663
 
-// 80071028 88%  https://decomp.me/scratch/nhecQ GFXSpawn
+// 80071028 95%  https://decomp.me/scratch/nhecQ GFXSpawn
 // 800711DC 100% https://decomp.me/scratch/BfRr5 GFXSpawn_Skip
 // 8007121C 0%   https://decomp.me/scratch/j3w46 CreateHitbox
 // 800715EC 100%                                 CreateHitbox_Skip
 // 8007162C 100% https://decomp.me/scratch/FqOVR SetHitboxDamage
+
+
+
+
+
+
+
+
+// https://decomp.me/scratch/5kHNA
+
+// SSB64
+// https://github.com/VetriTheRetri/ssb-decomp/blob/e6fa7e2eb6a861e759eeb1c5e5ead0945d9df6a6/game/src/gm/gmscript.h#L8
+// https://github.com/VetriTheRetri/ssb-decomp/blob/e6fa7e2eb6a861e759eeb1c5e5ead0945d9df6a6/game/src/ovl2/fighter_think.c#L95
